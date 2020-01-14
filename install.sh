83e08ac78ed106c14656fb1e9ef6ba32a449cdc3
x1unix <denis0051@gmail.com>
2020-01-14 02:24:59 +0200

    installer: refactor

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ PKG_URL="github.com/x1unix/telshell"
 URL_DOWNLOAD_PREFIX="https://${PKG_URL}/releases/latest/download"
 ISSUE_URL="https://${PKG_URL}/issues"
 
+OS=$(uname -s | awk '{print tolower($0)}')
+ARCH=$(uname -m)
+
 RED="\033[0;31m"
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -18,24 +21,28 @@ panic() {
     exit 1
 }
 
-get_bin_name() {
-    os=$(uname -s | awk '{print tolower($0)}')
-    case $os in
+is_windows() {
+  case ${OS} in
     cygwin*|mingw32*|msys*|mingw*)
-      os=windows
-      file_suffix=.exe
+      return 0
       ;;
     *)
+      return 1
       ;;
     esac
+}
 
+get_download_url() {
     arc=$(get_arch)
-    echo "telshell_${os}-${arc}${file_suffix}"
+
+    if is_windows; then
+      file_suffix=.exe
+    fi
+    echo "${URL_DOWNLOAD_PREFIX}/telshell_${OS}-${arc}${file_suffix}"
 }
 
 get_arch() {
-    a=$(uname -m)
-    case ${a} in
+    case $ARCH in
     "x86_64" | "amd64" )
       echo "amd64"
       ;;
@@ -43,31 +50,32 @@ get_arch() {
       echo "i386"
       ;;
     *)
-      echo $a
+      echo "$ARCH"
       ;;
     esac
 }
 
-
 main() {
-    file_name=$(get_bin_name)
     download_dir="${HOME}/bin"
-    mkdir -p ${download_dir}
+    mkdir -p "${download_dir}"
 
     dest_file="${download_dir}/telshell"
-    download_url=${URL_DOWNLOAD_PREFIX}/${file_name}
-    echo "-> Downloading '${download_url}'..."
+    if is_windows; then
+      dest_file="${dest_file}.exe"
+    fi
 
-    http_status=$(curl --fail --write-out "%{http_code}" -L --show-error --progress -o ${dest_file} ${download_url})
+    download_url=$(get_download_url)
+    echo "-> Downloading '${download_url}'..."
+    http_status=$(curl --fail --write-out "%{http_code}" -L --show-error --progress -o "${dest_file}" "${download_url}")
     case ${http_status} in
     "200")
-      chmod +x ${dest_file}
+      chmod +x "${dest_file}"
       echo "-> Successfully installed to '${dest_file}'"
       printf "${GREEN}Done!${NC}\n"
       exit 0
       ;;
     "404")
-      sys_label="$(uname -s) $(uname -m)"
+      sys_label="${OS} ${ARCH}"
       panic "No prebuilt binaries available for ${sys_label}, try to check out release for your platform at https://${PKG_URL}/releases"
       ;;
     *)
